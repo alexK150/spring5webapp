53b6c46f1ceb49688edd46f7f2ce413f00b2da1f
Alex <akuzin20@gmail.com>
2019-06-10 15:15:34 +0300

    init v2 (over equals, hash, toString)

---
+++ src/main/java/com/example/spring5webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,49 @@
+package com.example.spring5webapp.bootstrap;
+
+import com.example.spring5webapp.model.Author;
+import com.example.spring5webapp.model.Book;
+import com.example.spring5webapp.repository.AuthorRepo;
+import com.example.spring5webapp.repository.BookRepo;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private AuthorRepo authorRepo;
+    private BookRepo bookRepo;
+
+    public DevBootstrap(AuthorRepo authorRepo, BookRepo bookRepo) {
+        this.authorRepo = authorRepo;
+        this.bookRepo = bookRepo;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData(){
+
+        //Eric
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepo.save(eric);
+        bookRepo.save(ddd);
+
+
+        //Rod
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox" );
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepo.save(rod);
+        bookRepo.save(noEJB);
+    }
+
+}
